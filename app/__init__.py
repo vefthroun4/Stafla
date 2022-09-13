d2da4e1fc36a2168f37e5336508e3c5d055841bf
sveinnoli <sveinnoli@hotmail.com>
2022-09-13 14:54:02 +0000

    Bæti við skýrslu um gagnagrunn og einföldum virkni á gagnagrunn

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from config import Config
 import os
 
@@ -7,12 +8,13 @@ import os
 from app.blueprints.home import home_blueprint
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app(config=Config):
     app = Flask(__name__)
     app.config.from_object(config)
 
-    # Register bp
+    # Register blueprints
     app.register_blueprint(home_blueprint)
 
     # Try to setup instance folder if it does not exist
@@ -20,7 +22,16 @@ def create_app(config=Config):
         os.makedirs(app.instance_path)
     except OSError:
         pass
-        
+
+    # Init modules        
     db.init_app(app)
+    migrate.init_app(app, db)
+
+    # allows db and User objects to be accessed from the "flask shell" command
+    #TODO Move to another folder specific for CLI commands
+    from app.models import User
+    @app.shell_context_processor
+    def make_shell_context():
+        return { "db": db, "User": User }
 
     return app
--- config.py
+++ config.py
@@ -5,5 +5,6 @@ from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config():
-    SECRET_KEY = os.environ.get("SECRET_KEY") or os.urandom(24)
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI") or "sqlite:///" + basedir + "\\instance" 
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "$up3r_dup3r_$3cr3t"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI") or "sqlite:///" + basedir + "\\instance\\app.db" 
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
