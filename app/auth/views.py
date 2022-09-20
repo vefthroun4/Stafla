28a25ec8644ff5be9b454db0ca152f2c74a423c1
elvaragust <elvarbesti@gmail.com>
2022-09-20 14:45:37 +0000

    signUp/LogIn

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,14 +1,13 @@
 from flask import render_template, flash, url_for, request
 from app.models import User
 from flask_login import login_user, logout_user, current_user
-from app.auth import auth
 from flask import Blueprint
 
 views = Blueprint('views', __name__)
 
 @auth.route("/login")
 def login():
-    return render_template("auth/login.html")
+    return render_template("auth/login.html", text="Testing", bollean = True)
 
 @auth.route("/signup")
 def register():
