1e283a1bcf87b0b150aec63968c8ac047192eacf
Karl Philip <karlpv213@gmail.com>
2022-09-20 10:55:02 +0000

    Fixed css

--- app/blueprints/home/views.py
+++ app/blueprints/home/views.py
@@ -1,6 +1,9 @@
 from app.blueprints.home import home
+from flask import Blueprint, render_template
 
+
+home = Blueprint("home", __name__, static_folder="static", template_folder="templates")
 # Create routes here
 @home.route("/")
 def index():
-    return "index"
+    return render_template("home/home.html")
