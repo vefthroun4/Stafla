d29d3ccf135ac7b388b43a4c4898adff6e293478
sveinnoli <sveinnoli@hotmail.com>
2022-09-21 00:53:37 +0000

    fixed auth routes

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,8 +2,7 @@ from flask import render_template, flash, url_for, request
 from app.models import User
 from flask_login import login_user, logout_user, current_user
 from flask import Blueprint
-
-views = Blueprint('views', __name__)
+from app.auth import auth
 
 @auth.route("/login")
 def login():
