796816efcb9c013f54d3d14b69b38533f306b722
sveinnoli <46562494+sveinnoli@users.noreply.github.com>
2022-09-22 13:40:09 +0000

    added registration for user

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,7 +3,9 @@ from app.models import User
 from flask_login import login_user, logout_user, current_user
 from werkzeug.urls import url_parse
 from app.auth import auth
-from app.auth.forms import LoginForm
+from app.auth.forms import LoginForm, RegistrationForm
+from app import db
+
 
 @auth.route("/login", methods=("GET", "POST"))
 def login():
@@ -13,7 +15,7 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user is None or not user.check_password(form.password.data):
-            flash("Invalid username or password")
+            flash("Invalid username or password", category="error")
             return redirect(url_for("auth.login"))
         login_user(user, remember=form.remember_me.data)
 
@@ -26,7 +28,18 @@ def login():
 
 @auth.route("/signup", methods=("GET", "POST"))
 def register():
-    return render_template("auth/signup.html")
+    if current_user.is_authenticated:
+        return redirect(url_for("home.index"))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("Successfully created an account.", category="success")
+        return redirect(url_for("auth.login"))
+        #TODO need to send confirmation email to confirm account        
+    return render_template("auth/signup.html", form=form)
 
 @auth.route("/logout")
 def logout():
